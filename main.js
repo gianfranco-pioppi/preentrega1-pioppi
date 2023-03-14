fc887e3b5f228ba1e406696d3a31c6bb46303eea
gianfranco-pioppi <gianfranco.pioppi02@gmail.com>
2023-03-14 19:06:27 -0300

    preentrega1

---
+++ main.js
@@ -0,0 +1,23 @@
+//
+
+//declaracion de variables
+
+// let notaUno = 8;
+// let notaDos = 8;
+
+// let promedio = (notaUno + notaDos) / 2;
+
+// console.log (promedio);
+
+// let golGol = prompt("ingrese algo");
+// console.log(golGol);
+
+// variables
+
+let usuario = prompt("Ingrese su usuario: ");
+let usuarioAutorizado = Gianfranco;
+let clave = prompt("Ingrese su clave: ")
+let claveAutorizada = 1234;
+
+
+
